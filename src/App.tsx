42e11db7b57fad3d67c76d78ca372b40f07179e3
Fabian5150 <fabian.strauch.04@web.de>
2023-07-11 23:16:36 +0200

    refactor extendig theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import { NativeBaseProvider, extendTheme } from "native-base";
 import Home from "./components/Home";
 
 const App: React.FC = () => {
-  const config = {
-    useSystemColorMode: false,
-    initialColorMode: 'dark',
-  };
+  const myTheme = extendTheme({
+    config: {
+      useSystemColorMode: false,
+      initialColorMode: 'dark',
+    }
+  });
 
   return (
     <BrowserRouter>
-      <NativeBaseProvider theme={extendTheme({ config })}>
+      <NativeBaseProvider theme={myTheme}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/start" element={<Home />} />
