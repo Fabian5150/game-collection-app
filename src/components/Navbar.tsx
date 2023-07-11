28994e5f0c42341878b6a3307f54ae909232f757
Fabian5150 <fabian.strauch.04@web.de>
2023-07-11 10:19:46 +0200

    darkmode in navbar component, tbd: transfer to whole app

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Row, Heading, HamburgerIcon, IconButton, CircleIcon, PlayIcon, Button } from "native-base";
+import { useColorMode, Row, Heading, HamburgerIcon, IconButton, CircleIcon, PlayIcon, Button, useColorModeValue } from "native-base";
 
 type Props = {
   heading: String
@@ -8,18 +8,22 @@ type Props = {
 
 const Navbar: React.FC<Props> = ({ heading }) => {
   const navigate = useNavigate();
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const text = useColorModeValue("Light", "Dark");
+  const bg = useColorModeValue("warmGray.50", "coolGray.800");
 
   return (
     <>
-      <Row bg="primary.50" alignItems="center" justifyContent="space-between" p="1">
+      <Row alignItems="center" justifyContent="space-between" p="1" bg={bg}>
         <Row alignItems="center">
           <IconButton
-            icon={<HamburgerIcon size="lg" color="darkText" />}
-            onPress={() => console.log("imagine menu here pls")}
+            icon={<HamburgerIcon size="lg" color={text} />}
+            onPress={() => console.log("imagine menu here pls", colorMode)}
           />
           <Button onPress={() => navigate('/start')} variant="ghost">
             <Row alignItems="center">
-              <PlayIcon size="md" color="darkText" />
+              <PlayIcon size="md" color={text} />
               <Heading size="md">GameCollection</Heading>
             </Row>
           </Button>
@@ -29,9 +33,12 @@ const Navbar: React.FC<Props> = ({ heading }) => {
         </Row>
         <Row alignItems="center">
           {/* placeholders */}
-          <IconButton icon={<CircleIcon size="lg" color="darkText" />} />
-          <IconButton icon={<CircleIcon size="lg" color="darkText" />} />
-          <IconButton icon={<CircleIcon size="lg" color="darkText" />} />
+          <IconButton
+            icon={<CircleIcon size="lg" color={text} />}
+            onPress={() => toggleColorMode()}
+          />
+          <IconButton icon={<CircleIcon size="lg" color={text} />} />
+          <IconButton icon={<CircleIcon size="lg" color={text} />} />
         </Row>
       </Row>
     </>
